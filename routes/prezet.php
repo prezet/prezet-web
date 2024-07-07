ed626056d1f604a0083a497ab439c4c1a3bb999d
Ben Bjurstrom <benbjurstrom@users.noreply.github.com>
2024-07-07 14:22:15 -0700

    Update cache headers

--- routes/prezet.php
+++ routes/prezet.php
@@ -4,7 +4,10 @@ use BenBjurstrom\Prezet\Http\Controllers\ImageController;
 use BenBjurstrom\Prezet\Http\Controllers\IndexController;
 use BenBjurstrom\Prezet\Http\Controllers\OgimageController;
 use BenBjurstrom\Prezet\Http\Controllers\ShowController;
+use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
+use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Support\Facades\Route;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
 
 Route::get('prezet/img/{path}', ImageController::class)
     ->name('prezet.image')
@@ -14,13 +17,20 @@ Route::get('/prezet/ogimage/{slug}', OgimageController::class)
     ->name('prezet.ogimage')
     ->where('slug', '.*');
 
-Route::get('/', IndexController::class)
-    ->name('prezet.index')
-    ->middleware('cache.headers:public;max_age=7200;etag');
-
-Route::get('{slug}', ShowController::class)
-    ->name('prezet.show')
-    ->where('slug', '.*')
-    // https://laravel.com/docs/11.x/routing#parameters-encoded-forward-slashes
-    ->middleware('cache.headers:public;max_age=7200;etag');
+Route::middleware('cache.headers:public;max_age=7200;etag')
     // Cloudflare free plan: Minimum Edge Cache TTL 2 hours
+    ->withoutMiddleware([
+        ShareErrorsFromSession::class,
+        StartSession::class,
+        ValidateCsrfToken::class,
+    ])
+    ->group(function () {
+        Route::get('/', IndexController::class)
+            ->name('prezet.index');
+
+
+        Route::get('{slug}', ShowController::class)
+            ->name('prezet.show')
+            ->where('slug', '.*');
+        // https://laravel.com/docs/11.x/routing#parameters-encoded-forward-slashes
+});
