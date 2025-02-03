4ec1cbaacdf41d742fb92eb6f4db021fe02547b5
Ben Bjurstrom <benbjurstrom@users.noreply.github.com>
2025-02-03 12:56:46 -0800

    update cache headers

--- routes/prezet.php
+++ routes/prezet.php
@@ -15,7 +15,7 @@ Route::withoutMiddleware([
         StartSession::class,
         ValidateCsrfToken::class,
     ])
-    ->middleware('cache.headers:public')
+    ->middleware('cache.headers:s_maxage=86400')
     ->group(function () {
         Route::get('prezet/search', SearchController::class)->name('prezet.search');
 
