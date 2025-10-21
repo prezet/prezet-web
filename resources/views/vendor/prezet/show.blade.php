b3dcffcca58b0b22126670ad4a3937bae18e4550
Ben Bjurstrom <benbjurstrom@users.noreply.github.com>
2025-10-20 20:38:51 -0700

    Update template

--- resources/views/vendor/prezet/show.blade.php
+++ resources/views/vendor/prezet/show.blade.php
@@ -1,4 +1,4 @@
-<x-prezet::template>
+<x-prezet.template :document="$document" :nav="$nav" :headings="$headings">
     @php
         seo()
         ->title($document->frontmatter->title)
@@ -12,77 +12,41 @@
         <script type="application/ld+json">{!! $linkedData !!}</script>
     @endpush
 
-    {{-- Right Sidebar --}}
-    <x-slot name="left">
-        <x-prezet::sidebar :nav="$nav" />
-    </x-slot>
+    <section class="mt-8 min-w-0 md:mr-8 xl:mr-0">
 
-    {{-- Main Content --}}
-    <article>
-        <header class="mb-9 space-y-1">
-            <p class="font-display text-sm font-medium text-primary-500">
-                {{ $document->category }}
-            </p>
-            <h1
-                class="font-display text-4xl font-medium tracking-tight text-stone-900"
+        <div class="relative">
+            <div class="space-y-2.5">
+                <div
+                    class="eyebrow text-primary dark:text-primary-light h-5 text-sm font-semibold"
+                >
+                    {{ $document->category }}
+                </div>
+
+                <div
+                    class="relative flex flex-col items-start gap-2 sm:flex-row sm:items-center"
+                >
+                    <h1
+                        class="inline-block text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl dark:text-gray-200"
+                    >
+                        {{ $document->frontmatter->title }}
+                    </h1>
+                </div>
+            </div>
+
+            <div
+                class="prose prose-gray dark:prose-invert mt-2 text-lg"
             >
-                {{ $document->frontmatter->title }}
-            </h1>
-        </header>
-        <div
-            class="prose-headings:font-display prose prose-stone max-w-none prose-a:border-b prose-a:border-dashed prose-a:border-black/30 prose-a:font-semibold prose-a:no-underline prose-a:hover:border-solid prose-img:rounded-sm"
+                <p>{{ $document->frontmatter->description }}</p>
+            </div>
+        </div>
+
+        <article
+            class="prose prose-gray dark:prose-invert relative mt-8 mb-14 !max-w-none"
         >
             {!! $body !!}
-        </div>
-    </article>
+        </article>
 
-    {{-- Right Sidebar --}}
-    <x-slot name="right">
-        <div
-            class="hidden xl:sticky xl:top-19 xl:-mr-6 xl:block xl:h-[calc(100vh-4.75rem)] xl:flex-none xl:overflow-y-auto xl:py-16 xl:pr-6"
-        >
-            <nav aria-labelledby="on-this-page-title" class="w-56">
-                <p
-                    id="on-this-page-title"
-                    class="font-display text-sm font-medium text-stone-900"
-                >
-                    On this page
-                </p>
-                <ol role="list" class="mt-4 space-y-3 text-sm">
-                    @foreach ($headings as $h2)
-                        <li>
-                            <a
-                                href="#{{ $h2['id'] }}"
-                                :class="{'text-primary-500 hover:text-primary-500': activeHeading === '#{{ $h2['title'] }}'}"
-                                x-on:click.prevent="scrollToHeading('{{ $h2['id'] }}')"
-                                class="transition-colors"
-                            >
-                                {{ $h2['title'] }}
-                            </a>
+        <x-prezet.footer />
+    </section>
 
-                            @if ($h2['children'])
-                                <ol
-                                    role="list"
-                                    class="mt-2 space-y-3 border-l pl-5"
-                                >
-                                    @foreach ($h2['children'] as $h3)
-                                        <li>
-                                            <a
-                                                href="#{{ $h3['id'] }}"
-                                                :class="{'text-primary-500 hover:text-primary-500': activeHeading === '#{{ $h3['title'] }}'}"
-                                                x-on:click.prevent="scrollToHeading('{{ $h3['id'] }}')"
-                                                class="transition-colors"
-                                            >
-                                                {{ $h3['title'] }}
-                                            </a>
-                                        </li>
-                                    @endforeach
-                                </ol>
-                            @endif
-                        </li>
-                    @endforeach
-                </ol>
-            </nav>
-        </div>
-    </x-slot>
-</x-prezet::template>
+</x-prezet.template>
